10c193a946dec115ab8c2d2d4051797dad50921e
IsaiasTolP <toledop.isaias@gmail.com>
2024-09-26 16:00:27 +0100

    Hacer el for

--- JavaSpotify/src/js/index.js
+++ JavaSpotify/src/js/index.js
@@ -2,11 +2,13 @@ import songs from "../assets/songs/*.mp3";
 
 Object.keys(songs);
 
-const map = { };
+const map = { }; // Diccionario clave-valor
 
 let aux = 1;
 for (var key of Object.keys(songs)) {
     // Asociar map[`.item-${aux}`] con la canción songs[key]
+    map[`.item-${aux}`] = key;
+    aux++;
 }
 
 // Crear objeto Player y pasar el objeto map
